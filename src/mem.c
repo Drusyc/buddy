53d089bb56deb7aa4887798af516690a9c8dd870
Pierre LEBOUCHER <pierre.leboucher@ensimag.grenoble-inp.fr>
2015-01-28 08:42:17 +0100

    initialisation du tableau des tailles

--- src/mem.c
+++ src/mem.c
@@ -14,6 +14,9 @@
 void *zone_memoire = 0;
 
 /* ecrire votre code ici */
+void * tzl[40];
+unsigned int size[40];
+int subBuddy[40];
 
 int 
 mem_init()
@@ -25,8 +28,20 @@ mem_init()
       perror("mem_init:");
       return -1;
     }
-
+    
+  /* ecrire notre code ici */  
+  size[0] = 1;
+  size[1] = 2;
+  size[2] = 3;
+  for(int i = 3; i < 40; i++) {
+      if(i%2 == 1) {
+          size[i] = 2*size[i-2];
+      } else {
+          size[i] = 3*size[i-3];
+      }
+  }
   /* ecrire votre code ici */
+  
 
   return 0;
 }
